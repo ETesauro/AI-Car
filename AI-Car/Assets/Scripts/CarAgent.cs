cf8f002b920441f68afd837a50dbd2a41ec39913
unknown <emmanel.tesaro@gmail.com>
2020-07-02 13:08:19 +0200

    Updated CarAgent script to increase reward assingment and learning speed

--- AI-Car/Assets/Scripts/CarAgent.cs
+++ AI-Car/Assets/Scripts/CarAgent.cs
@@ -23,18 +23,23 @@ public class CarAgent : Agent
     public Waypoint[] waypoints;
     private WaypointNavigator waypointNavigator;
 
+
+    private float maxZdist;
+    private float xDist, zDist;
+    private bool xCheck, zCheck;
+    private Vector3 toTarget;
+    private bool manualBrake = false, pedoneCheck;
+
     public override void Initialize()
     {
         rBody = GetComponent<Rigidbody>();
         waypointNavigator = pedone.GetComponent<WaypointNavigator>();
     }
-    
+
     public override void OnEpisodeBegin()
     {
         ResetCar();
         ResetPedone();
-
-        Debug.Log(GetCumulativeReward());
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -64,20 +69,24 @@ public class CarAgent : Agent
         //ferma la macchina
         if (Input.GetKey(KeyCode.Space))
         {
-            actionsOut[2] = 0f;
+            actionsOut[2] = 1f;
         }
         else
-            actionsOut[2] = 1f;
+            actionsOut[2] = 0f;
 
     }
 
     public override void OnActionReceived(float[] vectorAction)
     {
+        // vectorAction[0] -> destra/sinistra
+        // vectorAction[1] -> avanti/dietro
+        // vectorAction[2] -> frena
+
         if (vectorAction[1] == 0 || Input.GetKey(KeyCode.Space))
         {
             brakes = 15000;
         }
-        else if(vectorAction[1] >= 0)
+        else if (vectorAction[1] >= 0)
         {
             brakes = 0;
             WheelCFR.motorTorque = vectorAction[1] * motorForce;
@@ -87,22 +96,58 @@ public class CarAgent : Agent
             AddReward(0.1f);
         }
 
-        WheelCFR.brakeTorque = brakes;
-        WheelCFL.brakeTorque = brakes;
-        WheelCBR.brakeTorque = brakes;
-        WheelCBL.brakeTorque = brakes;
-
-        //ferma immediatamente la macchina
-        /*if (vectorAction[2] <= 0.5f)
+        if (vectorAction[2] >= 0.5)
         {
-            rBody.velocity = new Vector3(vectorAction[2], vectorAction[2], vectorAction[2]);
-            rBody.angularVelocity = new Vector3(vectorAction[2], vectorAction[2], vectorAction[2]);
-        }*/
+            manualBrake = true;
+            frena(1f);
+        }
+        else manualBrake = false;
 
-        //Se va indietro assegno un bonus negativo
-        if (vectorAction[1] < 0)
+        pedoneCheck = CheckPedoni();
+
+        if (vectorAction[1] < 0 && !pedoneCheck)
         {
+            AddReward(-5f);
+        }
+        else
+            AddReward(0.1f);
+
+        if (vectorAction[1] > 0 && !pedoneCheck) AddReward(10f);
+
+        if (pedoneCheck)
+        {
+            if (getVelocitySpeed() < 2 && getVelocitySpeed() > -1)
+            {
+                Debug.Log("Correct Speed");
+                AddReward(maxZdist * 100f);
+            }
+            else
+            {
+                AddReward(-100f);
+            }
+        }
+        else
+        {
+            if (getVelocitySpeed() < 3 && getVelocitySpeed() > -3)
+            {
+
+                AddReward(-1000f);
+            }
+            else AddReward(0.5f);
+        }
+
+        if (!pedoneCheck && manualBrake)
+        {
+
+            Debug.Log("Wrong Brake");
+
             AddReward(-10f);
+        }
+
+        if (pedoneCheck && manualBrake)
+        {
+            Debug.Log("Correct Brake");
+            AddReward(10f);
         }
     }
 
@@ -135,6 +180,75 @@ public class CarAgent : Agent
         trans.rotation = wheelQuant;
     }
 
+    private bool CheckPedoni()
+    {
+
+        maxZdist = 0;
+        bool pedOnTrajectory = false;
+        toTarget = (pedone.transform.position - transform.position).normalized;
+
+        if (Vector3.Dot(toTarget, transform.forward) > 0)
+        {
+            xDist = (pedone.transform.position.x - transform.position.x) + 10;
+            zDist = Mathf.Abs(pedone.transform.position.z - transform.position.z);
+
+            xCheck = (-2 < xDist) && (xDist < 14);
+            zCheck = (0 < zDist) && (zDist < 10);
+
+            //Debug.Log("xCheck: " + xCheck + ", distanza: " + xDist);
+            //Debug.Log("zCheck: " + zCheck + ", distanza: " + zDist);
+
+            if (xCheck && zCheck)
+            {
+                Debug.Log("pedastrian " + pedone.name + " is in a risk point for " + gameObject.name + " .");
+                Debug.DrawLine(transform.position, pedone.transform.position, Color.blue);
+            }
+
+            if ((xCheck && zCheck) && !pedOnTrajectory)
+            {
+                if (zDist > maxZdist) maxZdist = zDist;
+                pedOnTrajectory = true;
+            }
+        }
+
+        return pedOnTrajectory;
+    }
+
+    protected float getVelocitySpeed()
+    {
+        //Debug.Log("velocity: " + transform.InverseTransformDirection(rBody.velocity).z);
+        return transform.InverseTransformDirection(rBody.velocity).z;
+    }
+
+    protected void frena(float decrement)
+    {
+        WheelCFR.brakeTorque = brakes;
+        WheelCFL.brakeTorque = brakes;
+        WheelCBR.brakeTorque = brakes;
+        WheelCBL.brakeTorque = brakes;
+        diminuisciVelocita(1f, decrement);
+    }
+
+    protected void diminuisciVelocita(float toCheck, float decrement)
+    {
+        if ((rBody.velocity.x > toCheck || rBody.velocity.x < toCheck * (-1)) || (rBody.velocity.y > toCheck || rBody.velocity.y < toCheck * (-1))
+            || (rBody.velocity.z > toCheck || rBody.velocity.z < toCheck * (-1)))
+        {
+            if (rBody.velocity.x > toCheck)
+                rBody.velocity = new Vector3(rBody.velocity.x - decrement, rBody.velocity.y, rBody.velocity.z);
+            if (rBody.velocity.x < toCheck * (-1))
+                rBody.velocity = new Vector3(rBody.velocity.x + decrement, rBody.velocity.y, rBody.velocity.z);
+            if (rBody.velocity.y > toCheck)
+                rBody.velocity = new Vector3(rBody.velocity.x, rBody.velocity.y - decrement, rBody.velocity.z);
+            if (rBody.velocity.y < toCheck * (-1))
+                rBody.velocity = new Vector3(rBody.velocity.x, rBody.velocity.y + decrement, rBody.velocity.z);
+            if (rBody.velocity.z > toCheck)
+                rBody.velocity = new Vector3(rBody.velocity.x, rBody.velocity.y, rBody.velocity.z - decrement);
+            if (rBody.velocity.z < toCheck * (-1))
+                rBody.velocity = new Vector3(rBody.velocity.x, rBody.velocity.y, rBody.velocity.z + decrement);
+        }
+    }
+
     // --------- FINE CONTROLLI MACCHINA ---------
 
     public void FixedUpdate()
@@ -148,7 +262,7 @@ public class CarAgent : Agent
     {
         if (other.tag == "Checkpoint")
         {
-            AddReward(1.0f);
+            AddReward(100.0f);
             EndEpisode();
         }
 
@@ -162,6 +276,19 @@ public class CarAgent : Agent
         {
             AddReward(-0.1f);
             EndEpisode();
+        }
+
+        if (other.tag == "Strisce")
+        {
+            AddReward(0.1f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Strisce")
+        {
+            AddReward(100f);
         }
     }
 
